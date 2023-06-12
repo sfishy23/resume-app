0bca8390db83bd4c8f5b43265150fd9bf5ca4465
Shaun Kearley <sfishy666@gmail.com>
2023-06-12 15:37:11 +0200

    testing the secrets within pages env

--- src/app/(main)/spaceport/forecast/page.tsx
+++ src/app/(main)/spaceport/forecast/page.tsx
@@ -1,7 +1,29 @@
+"use client";
 import React from "react";
 
 export default function Forecast() {
+   const test = "cape town";
+   // async function fetchWeatherResults() {
+   //    const res = await fetch(
+   //       `${process.env.BASE_URI}weather?q=${test}&units=metric&APPID=${process.env.WEATHER_API_KEY}`
+   //    );
+   //    console.log(res.json);
+   // }
    return (
-      <div className="w-full flex justify-center items-center">Forecast</div>
+      <div className="w-full flex justify-center items-center">
+         <div className="flex flex-col">
+            <h1>Forecast</h1>
+            {/* <input /> */}
+            {/* <button type="button" onClick={() => fetchWeatherResults()}>
+               TEST
+            </button> */}
+            <button
+               type="button"
+               onClick={() => console.log(process.env.NEXT_PUBLIC_TEST_SECRET)}
+            >
+               TEST
+            </button>
+         </div>
+      </div>
    );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
       <html lang="en">
          <body
             style={{
-               backgroundImage: `url(${process.env.FIXED_URL}/mainBG.jpg)`,
+               backgroundImage: `url(${process.env.NEXT_PUBLIC_FIXED_URL}/mainBG.jpg)`,
                backgroundSize: "auto",
                backgroundRepeat: "no-repeat",
             }}
